288e730116862c9778247e6d812f3034a034a3f6
JorgeGalicia117 <jagalicia27@gmail.com>
2025-10-22 17:01:24 -0600

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hola, soy Jorge Galicia</h1>
+      <h2>Carné: 202404125</h2>
+      <p>¡Bienvenido a mi app creada con React + Vite + Bun!</p>
+    </div>
+  );
+}
+
+export default App;
